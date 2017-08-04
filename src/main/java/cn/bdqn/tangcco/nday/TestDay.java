448e06b96d032abcb619bdc9af8fb39da1f6a1d5
zhounbing1 <1071594554@qq.com>
2017-08-04 12:26:23 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/nday/TestDay.java
@@ -0,0 +1,16 @@
+package cn.bdqn.tangcco.nday;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class TestDay {
+
+    @Test
+
+    public void testSynHello(){
+        System.out.println("Hello!World");
+        
+    }
+}
